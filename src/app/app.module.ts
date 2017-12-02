223bbc1e326f17fce09cccc8ce3b2dd8b5af07a9
Anderson <andersonsrsilva@gmail.com>
2017-12-02 15:55:26 -0200

    Into

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
+import { IntoPage } from '../pages/into/into';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -11,10 +12,12 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
+    IntoPage,
     ContactPage,
     HomePage,
     TabsPage
@@ -27,6 +30,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
+    IntoPage,
     ContactPage,
     HomePage,
     TabsPage
