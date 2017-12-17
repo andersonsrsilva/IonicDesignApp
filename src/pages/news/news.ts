9ed47eb6ba0219aee79a3bc6ffa668c06b3e0971
Anderson <andersonsrsilva@gmail.com>
2017-12-17 16:24:07 -0200

    Config

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the NewsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-news',
@@ -18,8 +11,5 @@ export class NewsPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad NewsPage');
-  }
 
 }
