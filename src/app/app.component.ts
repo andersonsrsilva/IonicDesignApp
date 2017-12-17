6d69584eafbecd7d366b083e411d87fb46d7dafe
Anderson <andersonsrsilva@gmail.com>
2017-12-17 17:58:25 -0200

    News

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { IntoPage } from '../pages/into/into';
+import { NewsPage } from '../pages/news/news';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = IntoPage;
+  rootPage:any = NewsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
